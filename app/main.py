ab0f82b57d7c99e65b965430d8623e1fdebc03b1
Sumit Parya <sumitparya2004@gmail.com>
2024-06-10 03:35:07 +0530

    pass stage4

--- app/main.py
+++ app/main.py
@@ -11,12 +11,16 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept()
     print("Received connection from", addr[0], "port", addr[1])
-    data = conn.recv(1024).decode("utf-8")
+    data = conn.recv(1024).decode()
     path = data.split(" ")[1]
     if path == "/":
-        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        pass
+    elif "/echo/" in path:
+        text = path.split("/")[2]
+        print(text)
+        conn.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(text)}\r\n\r\n{text}\r\n".encode())
     else:
-        conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 if __name__ == "__main__":
     main()
